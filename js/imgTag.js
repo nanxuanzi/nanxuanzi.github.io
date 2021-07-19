96ae115236c4b9e08d21c2f64aa339c9f2a92705
nanxuanzi <878216066@qq.com>
2021-07-19 20:32:02 +0800

    .

--- js/imgTag.js
+++ js/imgTag.js
@@ -5,7 +5,7 @@
 //获取json对象
 $(function(){
     //get请求json https://cdn.jsdelivr.net/gh/nanxuanzi/Images@master
-    $.get('https://cdn.jsdelivr.net/gh/nanxuanzi/Images@master/json/Images.json',(data,status)=>{
+    $.get('https://cdn.jsdelivr.net/gh/nanxuanzi/nanxuanzi.github.io@master/json/Images.json',(data,status)=>{
         if(status='success'){
             console.log('请求json成功！')
             //h data.img.h
